150f02d84b1d1b71e4a7911ec0fb97db0eca8e68
indigoxela <xela@indigofloat.at>
2023-09-23 10:19:59 +0200

    Issue #10: Add profile builder tool (#11)

---
+++ js/tinymce-dragdrop.js
@@ -0,0 +1,46 @@
+(function ($, Backdrop) {
+  "use strict";
+  Backdrop.behaviors.tinymceAdminDragdrop = {
+    attach: function (context, settings) {
+      $('#buttons-active').sortable({
+        connectWith: '#buttons-available',
+        items: '.tinybutton',
+        stop: function () {
+          updateFormItem();
+        }
+      });
+      $('#buttons-available').sortable({
+        connectWith: '#buttons-active',
+        items: '.tinybutton',
+        stop: function () {
+          updateFormItem();
+        }
+      });
+      $('#buttons-space .tinybutton').draggable({
+        connectToSortable: '#buttons-active',
+        helper: 'clone',
+        stop: function () {
+          updateFormItem();
+        }
+      });
+
+      let updateFormItem = function () {
+        setTimeout(function () {
+          let toolbarconf = '';
+          $('#buttons-active .tinybutton').each( function (i) {
+            if (i > 0) {
+              toolbarconf += ' ';
+            }
+            if (this.id) {
+              toolbarconf += this.id;
+            }
+            else if (this.classList.contains('separator')) {
+              toolbarconf += '|';
+            }
+          });
+          $('#edit-toolbar').val(toolbarconf);
+        }, 500);
+      }
+    }
+  }
+})(jQuery, Backdrop);
