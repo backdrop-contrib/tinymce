0624f046055ecea74837937f7fcc3038fb0abed3
indigoxela <user@olga.indigofloat.at>
2023-12-16 10:21:02 +0100

    Switch to IIFE

--- js/plugins/backdroplistprop/plugin.js
+++ js/plugins/backdroplistprop/plugin.js
@@ -2,103 +2,107 @@
  * @file
  * Backdrop TinyMCE list properties plugin.
  */
-'use strict';
+(function () {
+  'use strict';
 
-tinymce.PluginManager.add('backdroplistprop', function(editor, url) {
-  editor.ui.registry.addMenuItem('backdroplistorder', {
-    icon: 'ordered-list',
-    text: 'List properties...',
-    onAction: function () {
-      backdropListProp.openDialog(editor);
+  /**
+   * Opens a TinyMCE dialog with form content.
+   *
+   * @param object editor
+   *   TinyMCE instance.
+   */
+  const openDialog = function (editor) {
+    let parentOl = getOlParent(editor.selection.getNode());
+    if (!parentOl) {
+      return;
     }
-  });
-
-  editor.ui.registry.addContextMenu('backdroplistprops', {
-    update: function (element) {
-      if (backdropListProp.getOlParent(element)) {
-        return 'backdroplistorder';
-      }
-      return '';
-    }
-  });
-});
-
-const backdropListProp = {};
-
-/**
- * @param object editor
- *   TinyMCE instance.
- */
-backdropListProp.openDialog = function (editor) {
-  let parentOl = backdropListProp.getOlParent(editor.selection.getNode());
-  if (!parentOl) {
-    return;
-  }
-  editor.windowManager.open({
-    title: 'List properties...',
-    body: {
-      type: 'panel',
-      items: [
+    editor.windowManager.open({
+      title: 'List properties...',
+      body: {
+        type: 'panel',
+        items: [
+          {
+            type: 'input',
+            name: 'startnum',
+            label: 'Start list at number',
+            inputMode: 'numeric'
+          },
+          {
+            type: 'checkbox',
+            name: 'reverseorder',
+            label: 'Reversed order'
+          }
+        ]
+      },
+      initialData: {
+        startnum: editor.dom.getAttrib(parentOl, 'start', '1'),
+        reverseorder: parentOl.hasAttribute('reversed')
+      },
+      buttons: [
         {
-          type: 'input',
-          name: 'startnum',
-          label: 'Start list at number',
-          inputMode: 'numeric'
+          type: 'cancel',
+          name: 'cancel',
+          text: 'Cancel'
         },
         {
-          type: 'checkbox',
-          name: 'reverseorder',
-          label: 'Reversed order'
+          type: 'submit',
+          name: 'save',
+          text: 'Save',
+          primary: true
         }
-      ]
-    },
-    initialData: {
-      startnum: editor.dom.getAttrib(parentOl, 'start', '1'),
-      reverseorder: parentOl.hasAttribute('reversed')
-    },
-    buttons: [
-      {
-        type: 'cancel',
-        name: 'cancel',
-        text: 'Cancel'
-      },
-      {
-        type: 'submit',
-        name: 'save',
-        text: 'Save',
-        primary: true
+      ],
+      onSubmit: function (api) {
+        const data = api.getData();
+        if (data.reverseorder) {
+          parentOl.setAttribute('reversed', 'reversed');
+        }
+        else {
+          parentOl.removeAttribute('reversed');
+        }
+        if (data.startnum > 1) {
+          parentOl.setAttribute('start', data.startnum);
+        }
+        else {
+          parentOl.removeAttribute('start');
+        }
+        api.close();
       }
-    ],
-    onSubmit: function (api) {
-      const data = api.getData();
-      if (data.reverseorder) {
-        parentOl.setAttribute('reversed', 'reversed');
-      }
-      else {
-        parentOl.removeAttribute('reversed');
-      }
-      if (data.startnum > 1) {
-        parentOl.setAttribute('start', data.startnum);
-      }
-      else {
-        parentOl.removeAttribute('start');
-      }
-      api.close();
-    }
-  });
-}
-
-/**
- * Get the Dom parent OL if exists.
- *
- * @param HTMLElement node
- *
- * @return HTMLElement|false
- */
-backdropListProp.getOlParent = function (node) {
-  const closestLi = node.closest('li');
-  if (closestLi && closestLi.parentNode.nodeName == 'OL') {
-    return closestLi.parentNode;
+    });
   }
-  return false;
-}
+
+  /**
+   * Get the Dom parent OL if exists.
+   *
+   * @param HTMLElement node
+   *
+   * @return HTMLElement|false
+   */
+  const getOlParent = function (node) {
+    const closestLi = node.closest('li');
+    if (closestLi && closestLi.parentNode.nodeName == 'OL') {
+      return closestLi.parentNode;
+    }
+    return false;
+  }
+
+  // Register plugin.
+  tinymce.PluginManager.add('backdroplistprop', function(editor, url) {
+    editor.ui.registry.addMenuItem('backdroplistorder', {
+      icon: 'ordered-list',
+      text: 'List properties...',
+      onAction: function () {
+        openDialog(editor);
+      }
+    });
+
+    editor.ui.registry.addContextMenu('backdroplistprops', {
+      update: function (element) {
+        if (getOlParent(element)) {
+          return 'backdroplistorder';
+        }
+        return '';
+      }
+    });
+  });
+
+})();
