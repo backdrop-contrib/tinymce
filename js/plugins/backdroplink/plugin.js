aef8eabc40d2b3dfff75ef81d6455b47eabb7746
indigoxela <user@olga.indigofloat.at>
2024-07-18 09:14:56 +0200

    Issue #125: Selecting a linked image should activate both buttons

--- js/plugins/backdroplink/plugin.js
+++ js/plugins/backdroplink/plugin.js
@@ -136,7 +136,7 @@
         editor.on('SelectionChange', function () {
           let node = editor.selection.getNode();
           // The anchor plugin marks its links as not editable.
-          if (node.nodeName === 'A' && node.isContentEditable) {
+          if (node.isContentEditable && (node.nodeName === 'A' || node.parentNode.nodeName === 'A')) {
             api.setActive(true);
           }
           else {
@@ -151,18 +151,6 @@
       onAction: function () {
         editor.execCommand('unlink');
       },
-      onSetup: function (api) {
-        api.setEnabled(false);
-        editor.on('SelectionChange', function () {
-          let node = editor.selection.getNode();
-          if (node.nodeName === 'A' || node.parentNode.nodeName === 'A') {
-            api.setEnabled(true);
-          }
-          else {
-            api.setEnabled(false);
-          }
-        });
-      }
     });
 
     editor.ui.registry.addMenuItem('backdroplink', {
